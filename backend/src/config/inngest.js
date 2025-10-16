8ab3048221b36da0a9c2f9ee8ee4a1488a23e322
RohitRMendigeri <rohitmendigeri@gmail.com>
2025-10-16 09:57:48 +0530

    auth implemented

---
+++ backend/src/config/inngest.js
@@ -0,0 +1,36 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./db";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "SLACK" });
+
+const syncFunction = inngest.createFunction(
+    {id:"sync-user"},
+    {event:"clerk/user.created"},
+    async ({event}) => {
+        await connectDB();
+
+        const {id , email_addresses, first_name, profile_image_url} = event.data; 
+
+        const newUser = {
+            clerkID : id,
+            email : email_addresses[0]?.email_address,
+            name : `${first_name || ""} ${last_name || ""}`,
+            image : profile_image_url
+        }
+        await User.create(newUser);
+    }
+);
+
+const deleteUserFunction = inngest.createFunction(
+    {id:"delete-user-from-db"},
+    {event:"clerk/user.deleted"},
+    async ({event}) => {
+        const {id} = event.data;
+        await User.deleteOne({clerkID: id});
+       // await deleteStreamUser(id.toString());
+    }
+);
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [syncFunction, deleteUserFunction];
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,30 @@
 import express from 'express';
 import {ENV} from './config/env.js'; 
 import {connectDB} from './config/db.js';
-
+import { clerkMiddleware } from "@clerk/express";
+import {functions, inngest} from './config/inngest.js';
+import { serve } from "inngest/express";
 // load env vars from backend/.env (if present)
 
 
 const app = express();
 
-// use PORT from env or fallback to 3000
-const PORT = ENV.PORT ;
+app.use(express.json()); // to parse json request body
+app.use(clerkMiddleware()); // req.auth will be available in the request object
+
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+// use PORT from env
+const PORT = ENV.PORT || 5001;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+
+
+
+
 
 app.listen(PORT, () =>{
   console.log(`server is running on port ${PORT}`);
